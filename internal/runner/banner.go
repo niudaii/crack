08ad640db553d4072788a646205fa2580bd44860
niudaii <243536998@qq.com>
2022-09-02 10:50:09 +0800

    refactor: banner version

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -9,10 +9,10 @@ const banner = `
 / /__/ /  / /_/ / /__/ / \
 \___/_/   \__,_/\___/_/|_| 
 
-	  v0.0.1 by zp857
+	  ` + Version + ` by zp857
 `
 
-const Version = `v0.0.1`
+const Version = `v2.0`
 
 func showBanner() {
 	gologger.Print().Msgf("%v\n", banner)
