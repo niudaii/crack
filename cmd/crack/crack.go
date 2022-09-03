6c4fd6fd1aca52e5a5298986e49fdf2c54e343a7
niudaii <243536998@qq.com>
2022-09-03 09:32:15 +0800

    refactor: README

--- cmd/crack/crack.go
+++ cmd/crack/crack.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/niudaii/crack/internal/runner"
 	"github.com/projectdiscovery/gologger"
+	"time"
 )
 
 func main() {
@@ -11,5 +12,8 @@ func main() {
 	if err != nil {
 		gologger.Fatal().Msgf("Could not create runner: %v", err)
 	}
+	start := time.Now()
+	gologger.Info().Msgf("当前时间: %v", start.Format("2006-01-02 15:04:05"))
 	newRunner.Run()
+	gologger.Info().Msgf("花费时间: %v", time.Since(start))
 }
