b6ad794765b5d293aa0bcfa124e15227335a261b
niudaii <243536998@qq.com>
2022-09-05 16:58:40 +0800

    refactor: change engine to runner

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Runner struct {
-	options *Options
-	runner  *crack.Runner
+	options     *Options
+	crackRunner *crack.Runner
 }
 
 func NewRunner(options *Options) (*Runner, error) {
-	runner, err := crack.NewRunner(options.Threads, options.Timeout, options.Delay, options.CrackAll, options.Silent)
+	crackRunner, err := crack.NewRunner(options.Threads, options.Timeout, options.Delay, options.CrackAll, options.Silent)
 	if err != nil {
 		return nil, fmt.Errorf("NewRunner err, %v", err)
 	}
 	runner := &Runner{
-		options: options,
-		runner:  runner,
+		options:     options,
+		crackRunner: crackRunner,
 	}
 	return runner, nil
 }
@@ -26,8 +26,8 @@ func NewRunner(options *Options) (*Runner, error) {
 func (r *Runner) Run() {
 	addrs := crack.ParseTargets(r.options.Targets)
 	addrs = crack.FilterModule(addrs, r.options.Module)
-	addrs = r.runner.CheckAlive(addrs)
-	results := r.runner.Run(addrs, r.options.UserDict, r.options.PassDict)
+	addrs = r.crackRunner.CheckAlive(addrs)
+	results := r.crackRunner.Run(addrs, r.options.UserDict, r.options.PassDict)
 	if len(results) > 0 {
 		gologger.Info().Msgf("爆破成功: %v", len(results))
 		for _, result := range results {
